ff71934bb5903161373d069e8d04c8d774e5f672
daniel.sy2 <daniel.sy2@600900B211DE402>
2018-04-04 11:59:47 +1000

    Minesweeper

---
+++ Assets/~Minesweeper2D/Scripts/Grid.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+    public class Grid : MonoBehaviour
+    {
+        public GameObject tilePrefab;
+        public int width = 10;
+        public int height = 10;
+        public float spacing = .155f;
+
+        private Tile[,] tiles;
+
+        Tile SpawnTile(Vector3 pos)
+        {
+            GameObject clone = Instantiate(tilePrefab);
+            clone.transform.position = pos;
+            Tile currentTile = clone.GetComponent<Tile>();
+            return currentTile;
+        }
+
+        void GenerateTiles()
+        {
+            tiles = new Tile[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Vector2 halfSize = new Vector2(width / 2, height / 2);
+                    Vector2 pos = new Vector2(x - halfSize.x, y - halfSize.y);
+                    pos.x += 0.5f;
+                    pos.y += 0.5f; 
+
+                    pos *= spacing;
+                    Tile tile = SpawnTile(pos);
+                    tile.transform.SetParent(transform);
+                    tile.x = x;
+                    tile.y = y;
+                    tiles[x, y] = tile;
+                }
+            }
+        }
+        void Start()
+        {
+            GenerateTiles();
+        }
+        public int GetAdjacentMineCountAt(Tile t)
+        {
+            int count = 0;
+            for (int x = -1; x <= 1; x++)
+            {
+                int desiredX = t.x + x;
+                int desiredy = t.y + y;
+                Tile currentTile = tiles[desiredX, desiredy];
+                if(currentTile.isMine)
+                {
+                    count++;
+                }
+
+            }
+            return count;
+        }
+        void Update()
+        {
+            
+
+        }
+        
+    }
+
+}
---
+++ Assets/~Minesweeper2D/Scripts/Tile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        public int x = 0;
+        public int y = 0;
+        public bool isMine = false;
+        public bool isRevealed = false;
+        [Header("References")]
+        public Sprite[] emptySprites;
+        public Sprite[] mineSprites;
+
+        private SpriteRenderer rend;
+
+        void Awake ()
+        {
+            rend = GetComponent<SpriteRenderer>();
+        }
+        void Start ()
+        {
+            isMine = Random.value < .05f;
+        }
+
+        public void Reveal(int adjacentMines, int MineState = 0)
+        {
+            isRevealed = true;
+            if (isMine)
+            {
+                rend.sprite = mineSprites[MineState];
+            }
+            else
+            {
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+
+    }
+
+
+}
